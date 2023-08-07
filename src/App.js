093147d79c00dce60d10530415391e28a02263b3
ik3nna <iamiykendukwu@gmail.com>
2023-08-07 22:49:17 +0100

    feat: Implementation of a simple counter app using @reducjs/toolkit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { actions } from "./store";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = ()=> {
+    dispatch(actions.increment())
+  }
+
+  const decrement = ()=> {
+    dispatch(actions.decrement());
+  }
+
+  const addBy10 = () => {
+    dispatch(actions.addBy(10))
+  }
+
   return (
     <div className="App">
-      
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={addBy10}>Add by 10</button>
     </div>
   );
 }
